a4c9d2d84787fa4da30ae2283d0bb97b0bfad35c
Zarud3 <khanon.boyd@gmail.com>
2020-04-20 11:21:03 -0600

    bruh

--- test.py
+++ test.py
@@ -1,3 +1,6 @@
-print("ted is wierd")
-color = input("what is your favorite color? ")
-print(color)
+joke="Why was 6 afraid of 7?"
+awnser="why?"
+joke2="because 7 commited mass child murder and held 9 hostage"
+print (joke)
+print (awnser)
+print (joke2)
