4bab60507a46c16d33e8e29ecdbd8e529e2236b3
Zarud3 <khanon.boyd@gmail.com>
2020-05-01 12:49:53 -0600

    bruh moment

---
+++ ifstatments.py
@@ -0,0 +1,6 @@
+age = input('how old are you > ')
+age = int(age)
+if age < 20:
+    print('you are too old')
+    print('shouldnt you be sorting papers or something?')
+    print('go back to playing with the dinosaurs')
